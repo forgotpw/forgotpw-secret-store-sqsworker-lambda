940d1a436e9f45c544127d9dd471f1cfaa68bb54
Doug Kerwin <dwkerwin@gmail.com>
2018-08-19 23:08:13 -0400

    renamed logger name

--- logger.js
+++ logger.js
@@ -11,7 +11,7 @@ if (process.stdout.isTTY) {
 }
 
 module.exports = bunyan.createLogger({
-    name: 'forgotpw-secret-store-sqsworker',
+    name: 'fpw-secret-store-sqsworker',
     streams: [
         {
             level: config.LOG_LEVEL,
